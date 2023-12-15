d61d3c5663f4091a9566f9dd7a1261328d6fff16
AbuShl123 <shoalievabubakr@gmail.com>
2023-12-15 23:20:44 +0600

    Finished Chapter 1

---
+++ src/java/chapter1/functional_programming/Example.java
@@ -0,0 +1,16 @@
+package java.chapter1.functional_programming;
+
+@FunctionalInterface // <-- adding this annotation is optional
+interface Sprint {
+   void sprint(int speed);
+}
+
+public class Example implements Sprint {
+   public void sprint(int speed) {
+	System.out.println("Animal is sprinting fast! " + speed);
+   }
+
+   public static void main(String[] args) {
+
+   }
+}
---
+++ src/java/chapter1/functional_programming/TrickyLambda.java
@@ -0,0 +1,18 @@
+package java.chapter1.functional_programming;
+
+import java.util.function.Predicate;
+
+public class TrickyLambda {
+
+	
+	// the following code has three syntax errors. Try to guess! 
+	/*
+	public void variables(int a) {
+		int b = 2;
+		Predicate<Integer> p1 = a -> {
+			int b = 0;
+			int c = 0;
+			return b == c;}
+	}
+	*/
+}
