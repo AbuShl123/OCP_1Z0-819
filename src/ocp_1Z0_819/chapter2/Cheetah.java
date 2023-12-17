5d4ac052e0ff70fbea841fe471366984dd8f0678
AbuShl123 <shoalievabubakr@gmail.com>
2023-12-17 19:27:26 +0600

    Started Chapter 2

---
+++ src/ocp_1Z0_819/chapter2/Cheetah.java
@@ -0,0 +1,12 @@
+package ocp_1Z0_819.chapter2;
+
+@Exercise() public class Cheetah {
+}
+
+@Exercise class Sloth {
+}
+
+@Exercise
+class ZooEmployee {
+
+}
---
+++ src/ocp_1Z0_819/chapter2/Exercise.java
@@ -0,0 +1,4 @@
+package ocp_1Z0_819.chapter2;
+
+public @interface Exercise {
+}
