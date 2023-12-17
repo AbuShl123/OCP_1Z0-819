b9dfdacb8e20720fd06ae24393b32475d1b2b579
AbuShl123 <shoalievabubakr@gmail.com>
2023-12-17 20:49:45 +0600

    More annotations

---
+++ src/ocp_1Z0_819/chapter2/ExerciseAdvanced.java
@@ -0,0 +1,6 @@
+package ocp_1Z0_819.chapter2;
+
+public @interface ExerciseAdvanced {
+    int hoursPerDay();
+    int startHour() default 6;
+}
---
+++ src/ocp_1Z0_819/chapter2/Zoo.java
@@ -0,0 +1,14 @@
+package ocp_1Z0_819.chapter2;
+
+@ExerciseAdvanced(hoursPerDay = 2)
+public class Zoo {
+}
+
+@ExerciseAdvanced(hoursPerDay = 1, startHour = 8)
+class Human {
+
+}
+
+@ExerciseAdvanced(startHour = 10, hoursPerDay = 5) class Lion {
+
+}
